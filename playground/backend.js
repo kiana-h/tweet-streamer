c63e854c61b96d36e81d7668dc9f9b86bf903f14
Kiana H <s.kiana.hosseini@gmail.com>
2020-11-15 12:47:54 -0800

    =remove playground folder

--- playground/backend.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const streamSample = require("./streamSample");
-const streamSamplev1 = require("./streamSamplev1");
-
-// streamSample().catch(console.log);
-
-streamSamplev1().catch(console.log);
