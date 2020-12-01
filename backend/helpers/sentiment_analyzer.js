eb2c5cd04ebf7c0ec2f952222c832543ec0ede16
Kiana H <s.kiana.hosseini@gmail.com>
2020-11-30 17:14:42 -0800

    Test sentiment analyzer

--- backend/helpers/sentiment_analyzer.js
+++ backend/helpers/sentiment_analyzer.js
@@ -3,10 +3,10 @@ const SentimentSpanish = require("sentiment-spanish");
 const SentimentTurkish = require("sentiment-turkish");
 const SentimentFrench = require("sentiment-french");
 const SentimentPortuguese = require("sentiment-ptbr");
-const SentimentChinese = require("sentiment-zh_cn");
+// const SentimentChinese = require("sentiment-zh_cn");
 const SentimentSwedish = require("sentiment-swedish");
 const SentimentPolish = require("sentiment-polish");
-const SentimentGerman = require("ml-sentiment")({ lang: "de" });
+// const SentimentGerman = require("ml-sentiment")({ lang: "de" });
 const EmojiSentiment = require("wink-sentiment");
 const NaturalAnalyzer = require("natural").SentimentAnalyzer;
 const stemmer = require("natural").PorterStemmer;
@@ -37,10 +37,12 @@ const getSentimentScore = (text, lang) => {
   } else if (lang === "pl") {
     const result = SentimentPolish(text);
     return isNaN(result.score) ? null : result.score;
-  } else if (lang === "zh-cn") {
-    const result = SentimentChinese(text);
-    return isNaN(result.score) ? null : result.score;
-  } else if (lang === "it") {
+  }
+  // else if (lang === "zh-cn") {
+  //   const result = SentimentChinese(text);
+  //   return isNaN(result.score) ? null : result.score;
+  // }
+  else if (lang === "it") {
     const analyzer = new NaturalAnalyzer("Italian", stemmer, "pattern");
     const textArray = text.split(" ");
     return analyzer.getSentiment(textArray);
