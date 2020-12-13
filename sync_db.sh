3c02fd9a4c46de633abf01fb6e5829871e504841
Kiana H <s.kiana.hosseini@gmail.com>
2020-12-12 22:39:46 -0800

    Update sync_db.sh

--- sync_db.sh
+++ sync_db.sh
@@ -4,4 +4,4 @@ heroku pg:backups:capture DATABASE_URL
 heroku pg:backups:download
 psql -p 5432 "tweet_stream_er" -c "drop schema public cascade; create schema public;"
 pg_restore --verbose --clean --no-acl --no-owner -h localhost -U kiana -d tweet_stream_er latest.dump
-# rm latest.dump
+rm latest.dump
