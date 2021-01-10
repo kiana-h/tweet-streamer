1eefdb85cce79de833b1735b7f9b192b01723c6f
Kiana H <s.kiana.hosseini@gmail.com>
2021-01-09 19:42:34 -0800

    Remove comments

--- frontend/javascripts/components/root.jsx
+++ frontend/javascripts/components/root.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-// import Theme from "./theme/theme";
-// import { ThemeProvider } from "@material-ui/core/styles";
 import App from "./app";
 
 const Root = () => (
-  //   <Provider store={store}>
   <BrowserRouter>
-    {/* <ThemeProvider theme={Theme}> */}
     <App />
-    {/* </ThemeProvider> */}
   </BrowserRouter>
-  //   </Provider>
 );
 
 export default Root;
